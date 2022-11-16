1d630fda9c077ab1635fcd3acffd065f7ba5fc71
Harish Kute <112796233+Harrykute@users.noreply.github.com>
2022-11-16 23:49:28 +0530

    Add files via upload

---
+++ AvgThreeno.java
@@ -0,0 +1,22 @@
+public class AvgThreeno {
+
+   public static double avgthree(int a , int b, int c){
+        
+    int sum = a+b+c;
+
+    double avg=sum/3;
+
+
+    return avg;
+
+
+
+   }
+
+    public static void main(String[] args) {
+        
+
+        System.out.println(avgthree(5, 6, 7));
+    }
+    
+}
